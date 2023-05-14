de226499db285b466e1d73c0c3a8e2826943a7a1
Asadbek <asadbekrejabboyev0101@gmail.com>
2023-05-14 10:43:02 +0500

    some information hadd added to footer

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const request = axios.create({
-    baseURL: "http://16.16.171.16:8080/api/v1/"
+    baseURL: "https://jarvis-jon.uz:8443/api/v1/"
+    // baseURL: "http://16.16.171.16:8080/api/v1/"
 })
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,10 +1,32 @@
+import { Link, NavLink } from "react-router-dom";
 import "./assets/footer.scss";
+import { RouteLinks } from "../../api/AppDto";
 
 export default function Footer(){
     return (
         <footer>
-            <div className="container">
-            Footer
+            <div className="container d-flex justify-content-between">
+                <ul>
+                    <li><a target="blank" href="https://web.telegram.org/k/#@turgunovakaromatxon"><strong>Telegram :</strong> @turgunovakaromatxon</a></li>
+                    <li><a target="blank" href="https://www.instagram.com/turgunova_k/"><strong>Instagram :</strong> Turgunova_k</a></li>
+                    <li><a target="blank" href="+998998744098"><strong>Phone :</strong> +998998744098</a></li>
+                    <li><a target="blank" href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#advanced-search/to=turgunovakaromatxon%40gmail.com&query=in%3Asent&isrefinement=true&todisplay=turgunovakaromatxon%40gmail.com"><strong>Email:</strong> turgunovakaromatxon@gmail.com</a></li>
+                </ul>
+
+               <div className="footer-menu d-flex gap-4">
+                <div className="d-flex flex-column">
+                <Link to={RouteLinks.ExposureRoute}>Ma'ruzalar</Link>
+                <Link to={RouteLinks.PracticalWorkRoute}>Amaliy ishlar</Link>
+                <Link to={RouteLinks.IndependentWorkRoute}>Mustaqil ishlar</Link>
+                <Link to={RouteLinks.ArticleRoute}>Maqolalar</Link>
+                </div>
+                <div className="d-flex flex-column">
+                <Link to={RouteLinks.DGURoute}>DGU</Link>
+                <Link to={RouteLinks.MyInformationRoute}>Mening ma'lumotlarim</Link>
+                <Link to={RouteLinks.QualificationRoute}>Sertfikatlar</Link>
+                <Link to={RouteLinks.ImageRoute}>Rasmlar</Link>
+                </div>
+               </div>
             </div>
         </footer>
     )
